c4b03ab34419fa63960ee7b90dbbf3de34c10d2d
Przemyslaw Koziel <mos.koziel@gmail.com>
2023-03-09 19:41:16 +0100

    first commit

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { describe, test, expect } from 'vitest';
+import { render } from '@testing-library/react';
+import { App } from './App';
+
+describe('<App />', () => {
+  test('App mounts properly', () => {
+    const wrapper = render(<App />);
+    expect(wrapper).toBeTruthy();
+
+    const h1 = wrapper.container.querySelector('h1');
+    expect(h1?.textContent).toBe('Hello React');
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export const App = () => (
+  <h1 className="text-3xl font-bold underline">Hello React</h1>
+);
